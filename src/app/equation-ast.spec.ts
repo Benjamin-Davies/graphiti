f8b57f78d886dc11f7486eb1983678cad4cf3779
Benjamin-Davies <bentendavies@gmail.com>
2020-06-06 20:00:58 +1200

    Add more equation tests

--- src/app/equation-ast.spec.ts
+++ src/app/equation-ast.spec.ts
@@ -9,4 +9,15 @@ describe('EquationAst', () => {
     expect(ast.rootNode.children[0].children[0].sign).toBe('+');
     expect(ast.rootNode.children[0].children[0].children[0].value).toBe('x');
   });
+
+  it('should parse "y=2x" correctly', () => {
+    const ast = EquationAst.parse('y=2x');
+    expect(ast).toBeTruthy();
+    expect(ast.rootNode).toBeTruthy();
+
+    expect(ast.rootNode.type).toBe('equation');
+    expect(ast.rootNode.children[0].children[0].children[0].value).toBe('y');
+    expect(ast.rootNode.children[1].children[0].children[0].value).toBe(2);
+    expect(ast.rootNode.children[1].children[0].children[1].value).toBe('x');
+  });
 });
--- src/app/equation.spec.ts
+++ src/app/equation.spec.ts
@@ -4,4 +4,16 @@ describe('Equation', () => {
   it('should create an instance', () => {
     expect(new Equation()).toBeTruthy();
   });
+
+  it('should create an ast when text is set', () => {
+    let equation = new Equation();
+
+    expect(equation).toBeTruthy();
+    expect(equation.ast).toBeNull();
+
+    equation.text = 'y=2x';
+
+    expect(equation.ast).toBeTruthy();
+    expect(equation.ast.rootNode.type).toBe('equation');
+  });
 });
