19a6731df4cbaa700acdbf4337735595d365d2ba
Benjamin-Davies <bentendavies@gmail.com>
2020-06-07 21:16:57 +1200

    Fix tests for reduced asts

--- src/app/equation-ast.spec.ts
+++ src/app/equation-ast.spec.ts
@@ -1,23 +1,32 @@
-import { EquationAst } from './equation-ast';
+import { EquationAst, TermNode } from './equation-ast';
 
 describe('EquationAst', () => {
   it('should parse "x"', () => {
     const ast = EquationAst.parse('x');
     expect(ast).toBeTruthy();
     expect(ast.rootNode).toBeTruthy();
+    expect(ast.rootNode.type).toBe('equation');
+    expect(ast.rootNode.children.length).toBe(1);
 
-    expect(ast.rootNode.children[0].children[0].sign).toBe('+');
-    expect(ast.rootNode.children[0].children[0].children[0].value).toBe('x');
+    const term = ast.rootNode.children[0] as TermNode;
+    expect(term.type).toBe('term');
+    expect(term.sign).toBe('+');
+    expect(term.children.length).toBe(1);
+    expect(term.children[0].value).toBe('x');
   });
 
   it('should parse "y=2x" correctly', () => {
     const ast = EquationAst.parse('y=2x');
     expect(ast).toBeTruthy();
     expect(ast.rootNode).toBeTruthy();
-
     expect(ast.rootNode.type).toBe('equation');
-    expect(ast.rootNode.children[0].children[0].children[0].value).toBe('y');
-    expect(ast.rootNode.children[1].children[0].children[0].value).toBe(2);
-    expect(ast.rootNode.children[1].children[0].children[1].value).toBe('x');
+    expect(ast.rootNode.children.length).toBe(2);
+
+    const term0 = ast.rootNode.children[0] as TermNode;
+    expect(term0.children[0].value).toBe('y');
+
+    const term1 = ast.rootNode.children[1] as TermNode;
+    expect(term1.children[0].value).toBe(2);
+    expect(term1.children[1].value).toBe('x');
   });
 });
