e5e91a7686c09e2dfb1c86fe1d55184f131192b0
Benjamin-Davies <bentendavies@gmail.com>
2020-06-10 18:06:26 +1200

    Save and load equations from localStorage

--- src/app/equations.service.ts
+++ src/app/equations.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject, merge, Observable, OperatorFunction, scheduled, of } from 'rxjs';
-import { switchMap, startWith, map, mergeMap } from 'rxjs/operators';
+import { Subject, merge, Observable, OperatorFunction, scheduled, of, Subscription } from 'rxjs';
+import { switchMap, startWith, map, mergeMap, debounceTime } from 'rxjs/operators';
 
 import { Equation } from './equation';
 
@@ -12,13 +12,14 @@ export class EquationsService {
   private _equations: readonly Equation[] = [];
 
   public readonly arrayUpdates = new Subject<readonly Equation[]>();
-  public get updates(): Observable<Equation | readonly Equation[]> {
+  public get updates(): Observable<readonly Equation[]> {
     return this.arrayUpdates.pipe(
       startWith(this.equations),
       switchMap(a => of(
         ...a.map(e => e.updates),
       ).pipe(
-        mergeMap(x=>x),
+        mergeMap(x => x),
+        map(() => this.equations),
         startWith(a)
       ))
     )
@@ -31,6 +32,16 @@ export class EquationsService {
     this._equations = equations;
 
     this.arrayUpdates.next(equations);
+  }
+
+  private subscription: Subscription;
+  constructor() {
+    this.equations = loadEquations();
+    this.subscription = this.updates.pipe(debounceTime(500)).subscribe({ next: saveEquations });
+  }
+
+  destroy() {
+    this.subscription.unsubscribe();
   }
 
   addEquation(equation: Equation) {
@@ -46,4 +57,31 @@ export class EquationsService {
     this.equations = newEquations;
   }
 
+}
+
+const EQUATIONS_LOCALSTORAGE_KEY = 'equations';
+
+interface EquationSaveFormat {
+  text: string,
+}
+
+function loadEquations(): readonly Equation[] {
+  try {
+    const str = localStorage.getItem(EQUATIONS_LOCALSTORAGE_KEY);
+    const arr = JSON.parse(str) as EquationSaveFormat[];
+    return arr.map(({ text }) => {
+      const equation = new Equation();
+      equation.text = text;
+      return equation;
+    });
+  } catch (e) {
+    console.warn(e);
+    return [];
+  }
+}
+
+function saveEquations(equations: readonly Equation[]) {
+  const arr: EquationSaveFormat[] = equations.map(equation => ({ text: equation.text }));
+  const str = JSON.stringify(arr);
+  localStorage.setItem(EQUATIONS_LOCALSTORAGE_KEY, str);
 }
